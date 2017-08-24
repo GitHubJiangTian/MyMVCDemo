79c2652b28a2ed54bbbc4c9b382ac37ad1690f71
GitHubJiangTian <3208026665@qq.com>
2017-08-24 16:03:38 +0800

    before SpringMVC

--- src/com/company/dao/impl/DaoLoginRegistryImpl.java
+++ src/com/company/dao/impl/DaoLoginRegistryImpl.java
@@ -3,7 +3,6 @@ package com.company.dao.impl;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
@@ -23,21 +22,17 @@ public class DaoLoginRegistryImpl implements ILoginRegistryDao {
 	@Override
 	public void registry(Login login, Detail detail) throws Exception {
 		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
 		session.save(login);
 		session.save(detail);
-//		trans.commit();
 	}
 
 	@Override
 	public Login login(Login login) throws Exception {
 		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
 		Login result = null;
 		result = (Login) session.createQuery("from Login where username=:username and userpwd=:userpwd")
 				.setParameter("username", login.getUsername()).setParameter("userpwd", login.getUserpwd())
 				.uniqueResult();
-//		trans.commit();
 		return result;
 	}
 
--- src/com/company/dao/impl/DaoProductImpl.java
+++ src/com/company/dao/impl/DaoProductImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
@@ -12,77 +11,58 @@ import org.springframework.stereotype.Repository;
 import com.company.dao.idao.IProductDao;
 import com.company.dao.pojo.Product;
 
-
 @Repository("productDao")
 public class DaoProductImpl implements IProductDao {
 
 	@Autowired
 	@Qualifier("sessionFactory")
 	private SessionFactory sessionFactory;
-	
+
 	@Override
 	public void save(Product t) throws Exception {
-		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
-		session.save(t);
-//		trans.commit();
+		sessionFactory.getCurrentSession().save(t);
 	}
 
 	@Override
 	public void update(Product t) throws Exception {
-		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
-		session.update(t);
-//		trans.commit();
+		sessionFactory.getCurrentSession().update(t);
 	}
 
 	@Override
 	public void delete(Product t) throws Exception {
-		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
-		session.delete(t);
-//		trans.commit();
+		sessionFactory.getCurrentSession().delete(t);
 	}
 
 	@Override
 	public List<Product> findAll() throws Exception {
-		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
 		List<Product> productList = null;
-		//因为是正向工程，所以“from Product”中的Product不是数据库中的表名为Product表，而是自己pojo对应的实体累的名字
-		productList = session.createQuery("from Product").list();
-//		trans.commit();
+		// 因为是正向工程，所以“from Product”中的Product不是数据库中的表名为Product表，而是自己pojo对应的实体累的名字
+		productList = sessionFactory.getCurrentSession().createQuery("from Product").list();
 		return productList;
 	}
 
 	@Override
 	public Product findById(Integer k) throws Exception {
 		Session session = sessionFactory.openSession();
-//		Transaction trans = session.beginTransaction();
 		Product p = null;
-		p = (Product) session.createQuery("from Product where pid=:pid").setParameter("pid",k).uniqueResult();
-//		trans.commit();
+		p = (Product) session.createQuery("from Product where pid=:pid").setParameter("pid", k).uniqueResult();
 		session.close();
 		return p;
 	}
 
 	@Override
 	public List<Product> findByName(String pname) throws Exception {
-		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
 		List<Product> productList = null;
-		productList = session.createQuery("from Product where pname like :pname").setParameter("pname", "%"+pname+"%").list();
-//		trans.commit();
+		productList = sessionFactory.getCurrentSession().createQuery("from Product where pname like :pname")
+				.setParameter("pname", "%" + pname + "%").list();
 		return productList;
 	}
 
 	@Override
 	public List<Product> findByPage(int page, int size) throws Exception {
-		Session session = sessionFactory.getCurrentSession();
-//		Transaction trans = session.beginTransaction();
 		List<Product> productList = null;
-		productList = session.createQuery("from Product").setFirstResult((page - 1) * size).setMaxResults(size).list();
-//		trans.commit();
+		productList = sessionFactory.getCurrentSession().createQuery("from Product").setFirstResult((page - 1) * size)
+				.setMaxResults(size).list();
 		return productList;
 	}
 
